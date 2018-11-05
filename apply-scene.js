48034b797c71365a0c158d4a8d1f7ad9b399c752
Paolo Tremadio <hello@paolotremadio.com>
2018-11-05 20:15:26 +0000

    Limit number of requests to the Hue hub

--- apply-scene.js
+++ apply-scene.js
@@ -1,8 +1,33 @@
-const applySettingsToZone = async (hueApi, settings, zone) => {
+const Bottleneck = require("bottleneck");
+
+/*
+ * From https://developers.meethue.com/things-you-need-know
+ *
+ * Number of commands you can send per second
+ *
+ * You can send commands to the lights too fast. If you stay roughly around 10 commands per second to the /lights
+ * resource as maximum you should be fine. For /groups commands you should keep to a maximum of 1 per second.
+ *
+ *
+ * From https://developers.meethue.com/documentation/hue-system-performance
+ *
+ * As a general guideline we always recommend to our developers to stay at roughly 10 commands per second to the /lights
+ * resource with a 100ms gap between each API call. For /groups commands you should keep to a maximum of 1 per second.
+ * It is however always recommended to take into consideration the above information and to of course stress test your
+ * app/system to find the optimal values for your application.
+ */
+const limiter = new Bottleneck({
+  reservoir: 10, // initial value
+  reservoirRefreshAmount: 10,
+  reservoirRefreshInterval: 1000 // must be divisible by 250
+});
+
+const applySettingsToZone = (hueApi, settings, zone) => {
   zone.forEach(async (lamp) => {
     const { hueLightId } = lamp;
 
-    await hueApi.setLightState(hueLightId, settings);
+    const wrapped = limiter.wrap(hueApi.setLightState.bind(hueApi));
+    wrapped(hueLightId, settings);
   });
 };
 
@@ -15,7 +40,7 @@ module.exports = async (hueApi, sceneData, zones, logger) => {
         logger(`Error: zone "${zone}" not found`);
         return;
       }
-      await applySettingsToZone(hueApi, settings, zones[zone]);
+      applySettingsToZone(hueApi, settings, zones[zone]);
     });
   });
 };
